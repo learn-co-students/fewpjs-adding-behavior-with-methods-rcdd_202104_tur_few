1692a258878a01eccdc5034681cd4eb8e4177753
abduallahB <abduallah.imu2@gmail.com>
2021-05-20 17:34:35 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+class Cat {
+    //...
+    constructor(name , sex){
+        this.name = name
+        this.sex = sex
+    }
+
+     speak(){
+        return `${name} says meow!`
+    }
+  }
+   
+  class Dog {
+    //...
+    constructor(name , sex){
+        this.name = name
+        this.sex = sex
+    }
+    speak(){
+        return `${name} says woof!`
+    }
+  }
+   
+  class Bird {
+    //...
+    constructor(name , sex){
+        this.name = name
+        this.sex = sex
+    }
+    speak(){
+        if(this.sex === 'male')
+        return `It's me!  ${name} `
+        else
+        return ` ${name} says squawk! `
+    }
+  }
+
+  let cat = new Cat('lolo','male')
+  let dog = new Dog('rex','male')
+  let bird = new Bird('zajel','male')
