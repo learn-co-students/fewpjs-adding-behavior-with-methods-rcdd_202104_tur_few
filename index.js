61f4ffc00a2eb653f9dfb4e2711a6adbb85362ee
Ali Rıza <ars.lifestyle@hotmail.com>
2021-05-21 19:56:34 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+class Cat {
+    constructor(name, sex) {
+        this["name"] = name;
+        this["sex"] = sex;
+    };
+    speak() {
+        let result = `${this.name}` + " says meow!";
+        console.log(result);
+        return result;
+    };
+};
+
+class Dog {
+    constructor(name, sex) {
+        this["name"] = name;
+        this["sex"] = sex;
+    };
+    speak() {
+        let result = `${this.name}` + " says woof!";
+        console.log(result);
+        return result;
+    }
+};
+
+class Bird {
+    constructor(name, sex) {
+        this["name"] = name;
+        this["sex"] = sex;
+    };
+    speak() {
+        if (this.sex === "male") {
+            let result = "It's me! " + `${this.name}` + ", the parrot!";
+            console.log(result);
+            return result;
+        }
+        else if (this.sex === "female") {
+            let result = `${this.name}` + " says squawk!";
+            console.log(result);
+            return result;
+        }
+
+    }
+};
